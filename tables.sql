95d2c625e691da66fe0a3a90d2849f8d4ed59b3b
Tyler Fink <tfink419@ufl.edu>
2013-12-05 18:34:55 -0500

    Added Colons to data, Changer Airport Codes

--- tables.sql
+++ tables.sql
@@ -86,21 +86,21 @@ CREATE TABLE Payment
 
 --Data
 INSERT INTO Airport
-VALUES('0', 'Gainesville', 'Florida', 'Gainesville Regional Airport');
+VALUES('GAI', 'Gainesville', 'Florida', 'Gainesville Regional Airport');
 INSERT INTO Airport
-VALUES('1', 'Miami', 'Florida', 'Miami International Airport');
+VALUES('MIA', 'Miami', 'Florida', 'Miami International Airport');
 INSERT INTO Airport
-VALUES('2', 'Orlando', 'Florida', 'Orlando International Airport');
+VALUES('ORL', 'Orlando', 'Florida', 'Orlando International Airport');
 INSERT INTO Airport
-VALUES('3', 'Sarasota', 'Florida', 'Sarasota International Airport');
+VALUES('SAR', 'Sarasota', 'Florida', 'Sarasota International Airport');
 INSERT INTO Airport
-VALUES('4', 'West Palm Beach', 'Florida', 'Palm Beach International Airport');
+VALUES('PBI', 'West Palm Beach', 'Florida', 'Palm Beach International Airport');
 INSERT INTO Airport
-VALUES('5', 'Ft. Lauderdale', 'Florida', 'Ft. Lauderdale International Airport');
+VALUES('FTL', 'Ft. Lauderdale', 'Florida', 'Ft. Lauderdale International Airport');
 INSERT INTO Airport
-VALUES('6', 'New York City', 'New York', 'MLK International Airport');
+VALUES('MLK', 'New York City', 'New York', 'MLK International Airport');
 INSERT INTO Airport
-VALUES('7', 'New York City', 'New York', 'LaGuardia Airport');
+VALUES('LAG', 'New York City', 'New York', 'LaGuardia Airport');
 
 INSERT INTO Airplane
 VALUES('1', 'Boeing 747', '400');
@@ -112,58 +112,58 @@ INSERT INTO Airplane
 VALUES('4', 'Boeing 777', '380');
 
 INSERT INTO Trip
-VALUES('1', 'AA',500,'1', '2', 1);
+VALUES('1', 'AA',500,'GAI', 'MIA', 1);
 INSERT INTO Trip
-VALUES('2', 'Delta',300,'2', '3', 1);
+VALUES('2', 'Delta',300,'MIA', 'ORL', 1);
 INSERT INTO Trip
-VALUES('3', 'Delta',400,'3', '4', 2);
+VALUES('3', 'Delta',400,'ORL', 'SAR', 2);
 INSERT INTO Trip
-VALUES('4', 'AA',800,'1', '3', 2);
+VALUES('4', 'AA',800,'GAI', 'ORL', 2);
 INSERT INTO Trip
-VALUES('5', 'Canada',1200,'1', '4', 3);
+VALUES('5', 'Canada',1200,'GAI', 'SAR', 3);
 INSERT INTO Trip
-VALUES('6', 'AA',500,'2', '1', 1);
+VALUES('6', 'AA',500,'MIA', 'GAI', 1);
 INSERT INTO Trip
-VALUES('7', 'Delta',300,'3', '2', 1);
+VALUES('7', 'Delta',300,'ORL', 'MIA', 1);
 INSERT INTO Trip
-VALUES('8', 'Delta',400,'4', '3', 2);
+VALUES('8', 'Delta',400,'SAR', 'ORL', 2);
 INSERT INTO Trip
-VALUES('9', 'AA',800,'3', '1', 2);
+VALUES('9', 'AA',800,'ORL', 'GAI', 2);
 INSERT INTO Trip
-VALUES('10', 'Canada',1200,'4', '1', 3);
+VALUES('10', 'Canada',1200,'SAR', 'GAI', 3);
 
 INSERT INTO Flight_leg
-VALUES('1', '1','2','1', 400, '11-11-2013','0400','0600','1');
+VALUES('1', 'GAI','MIA','1', 400, '11-11-2013','4:00','0600','1');
 INSERT INTO Flight_leg
-VALUES('2', '2','3','2', 450, '11-11-2013','0630','0800','2');
+VALUES('2', 'MIA','ORL','2', 450, '11-11-2013','6:30','0800','2');
 INSERT INTO Flight_leg
-VALUES('3', '3','4','3', 400, '11-11-2013','0630','0800','3');
+VALUES('3', 'ORL','SAR','3', 400, '11-11-2013','6:30','0800','3');
 INSERT INTO Flight_leg
-VALUES('1', '1','2','4', 400, '11-11-2013','0400','0600','1');
+VALUES('1', 'GAI','MIA','4', 400, '11-11-2013','4:00','0600','1');
 INSERT INTO Flight_leg
-VALUES('2', '2','3','4', 450, '11-11-2013','0630','0800','2');
+VALUES('2', 'MIA','ORL','4', 450, '11-11-2013','6:30','0800','2');
 INSERT INTO Flight_leg
-VALUES('1', '1','2','5', 400, '11-11-2013','0400','0600','1');
+VALUES('1', 'GAI','MIA','5', 400, '11-11-2013','4:00','0600','1');
 INSERT INTO Flight_leg
-VALUES('2', '2','3','5', 450, '11-11-2013','0630','0800','2');
+VALUES('2', 'MIA','ORL','5', 450, '11-11-2013','6:30','0800','2');
 INSERT INTO Flight_leg
-VALUES('3', '3','4','5', 400, '11-11-2013','1000','1200','1');
+VALUES('3', 'ORL','SAR','5', 400, '11-11-2013','10:00','1200','1');
 INSERT INTO Flight_leg
-VALUES('4', '2','1','6', 400, '11-11-2013','0400','0600','1');
+VALUES('4', 'MIA','GAI','6', 400, '11-11-2013','4:00','0600','1');
 INSERT INTO Flight_leg
-VALUES('5', '3','2','7', 450, '11-11-2013','0630','0800','2');
+VALUES('5', 'ORL','MIA','7', 450, '11-11-2013','6:30','0800','2');
 INSERT INTO Flight_leg
-VALUES('6', '4','3','8', 400, '11-11-2013','0630','0800','3');
+VALUES('6', 'SAR','ORL','8', 400, '11-11-2013','6:30','0800','3');
 INSERT INTO Flight_leg
-VALUES('4', '2','1','9', 400, '11-11-2013','0400','0600','1');
+VALUES('4', 'MIA','GAI','9', 400, '11-11-2013','4:00','0600','1');
 INSERT INTO Flight_leg
-VALUES('5', '3','2','9', 450, '11-11-2013','0630','0800','2');
+VALUES('5', 'ORL','MIA','9', 450, '11-11-2013','6:30','0800','2');
 INSERT INTO Flight_leg
-VALUES('4', '2','1','10', 400, '11-11-2013','0400','0600','1');
+VALUES('4', 'MIA','GAI','10', 400, '11-11-2013','4:00','0600','1');
 INSERT INTO Flight_leg
-VALUES('5', '3','2','10', 450, '11-11-2013','0630','0800','2');
+VALUES('5', 'ORL','MIA','10', 450, '11-11-2013','6:30','0800','2');
 INSERT INTO Flight_leg
-VALUES('6', '4','3','10', 400, '11-11-2013','1000','1200','1');
+VALUES('6', 'SAR','ORL','10', 400, '11-11-2013','10:00','1200','1');
 
 
 --Triggers
